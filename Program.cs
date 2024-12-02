06ffddd268b1d0102a0600641447d3a44956d519
Juan Reyes <camiloreyes1125@gmail.com>
2024-12-01 23:46:10 -0500

    dog

--- Program.cs
+++ Program.cs
@@ -4,13 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(" .-. ");
-        Console.WriteLine("(o o)");
-        Console.WriteLine("| 0 |");
-        Console.WriteLine("|   |");
-        Console.WriteLine("'~~~'");
+        string name = "Shadow";
+        string breed = "Golden Retriever";
+        int age = 5;
+        double weight = 65.22;
+        bool spayed = true;
+
+      Console.WriteLine(name, breed, age, weight, spayed);  
+
     }
 }
 
 // Console.WriteLine() prints text to the console.
 // Console.ReadLine() captures user input in the console.
+
--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343082e0900db5b25cdae042cd9f65235086f7c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac0c6c1b81ab9b157f120fed74442a9f981732e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
